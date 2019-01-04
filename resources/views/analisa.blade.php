11295e66d4dc5e496c7ce1b15d9a97ecea2b7c9e
ata45 <34927815+ata45@users.noreply.github.com>
2019-01-04 07:31:29 +0700

    Nambahin home

--- resources/views/analisa.blade.php
+++ resources/views/analisa.blade.php
@@ -4,85 +4,85 @@
 
 @section('content')
 
-		 <!-- Main component for a primary marketing message or call to action -->
-         <div class="panel panel-primary">
-		  <!-- Default panel contents -->
-		  <div class="panel-heading">Analisa</div>
-		  <div class="panel-body">
-			<div>
-				<canvas id="canvas"></canvas>
-			</div>
-			<br />
-			<center>
-			<table>
-			<thead>
-                                <tr>
-                                    <th>
-                                        Nama
-                                    </th>
-                                    @foreach($kriteria as $k)
-                                        <th>{{ $k->kriteria  }}</th>
-                                    @endforeach
-                                </tr>
-                                </thead>
-								<tbody>
-                                @foreach($alternatif as $p)
-                                    <tr>
-                                        <td>{{ $p->alternatif  }}</td>
-                                        
-                                            @for($i=0;$i<$altcount;$i++)
-                                                <td>{{ $p->id_alternatif[$i] }}</td>
-                                            @endfor
-                                    </tr>
-                                @endforeach
-                                </tbody>
-			</table>
-			</center>
-		  </div>
-		</div>
+<!-- Main component for a primary marketing message or call to action -->
+<div class="panel panel-primary">
+  <!-- Default panel contents -->
+  <div class="panel-heading">Analisa</div>
+  <div class="panel-body">
+    <div>
+      <canvas id="canvas"></canvas>
+    </div>
+    <br />
+    <center>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              Nama
+            </th>
+            @foreach($kriteria as $k)
+            <th>{{ $k->kriteria  }}</th>
+            @endforeach
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($alternatif as $p)
+          <tr>
+            <td>{{ $p->alternatif  }}</td>
+
+            @for($i=0;$i<$altcount;$i++)
+            <td>{{ $p->id_alternatif[$i] }}</td>
+            @endfor
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </center>
+  </div>
+</div>
 
 
 @stop
 <script>
-	var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
 
-	var barChartData = {
-		labels : [
-			<?php 
-				for($i=0;$i<$altcount;$i++){
-					echo '"'.$alt_name[$i].'",';
-				}
-			?>
-		],
-		datasets : [
-			{
-				fillColor : "rgba(0,0,255,0.75)",
-				strokeColor : "rgba(220,220,220,0.8)",
-				highlightFill: "rgba(0,128,255,0.75)",
-				highlightStroke: "rgba(220,220,220,1)",
-				data : [
-					<?php 
-						for($i=0;$i<$altcount;$i++){
-							echo $vs[$i].',';
-						}
-					?>
-				]
-			},
-			/*{
-				fillColor : "rgba(151,187,205,0.5)",
-				strokeColor : "rgba(151,187,205,0.8)",
-				highlightFill : "rgba(151,187,205,0.75)",
-				highlightStroke : "rgba(151,187,205,1)",
-				data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
-			}*/
-		]
+var barChartData = {
+  labels : [
+    <?php
+    for($i=0;$i<$altcount;$i++){
+      echo '"'.$alt_name[$i].'",';
+    }
+    ?>
+  ],
+  datasets : [
+    {
+      fillColor : "rgba(0,0,255,0.75)",
+      strokeColor : "rgba(220,220,220,0.8)",
+      highlightFill: "rgba(0,128,255,0.75)",
+      highlightStroke: "rgba(220,220,220,1)",
+      data : [
+        <?php
+        for($i=0;$i<$altcount;$i++){
+          echo $vs[$i].',';
+        }
+        ?>
+      ]
+    },
+    /*{
+    fillColor : "rgba(151,187,205,0.5)",
+    strokeColor : "rgba(151,187,205,0.8)",
+    highlightFill : "rgba(151,187,205,0.75)",
+    highlightStroke : "rgba(151,187,205,1)",
+    data : [randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor(),randomScalingFactor()]
+  }*/
+]
 
-	}
-	window.onload = function(){
-		var ctx = document.getElementById("canvas").getContext("2d");
-		window.myBar = new Chart(ctx).Bar(barChartData, {
-			responsive : true
-		});
-	}
+}
+window.onload = function(){
+  var ctx = document.getElementById("canvas").getContext("2d");
+  window.myBar = new Chart(ctx).Bar(barChartData, {
+    responsive : true
+  });
+}
 
 </script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,12 +2,13 @@
 <html>
     <head>
         <title>App Name - @yield('title')</title>
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
         <link href="{{ asset('css/cerulean.min.css') }}" rel="stylesheet">
-    
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 	<!-- Datatables -->
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/datatables/dataTables.bootstrap.css') }}">
-	
+
 	    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 
         <script src="{{ asset('js/jquery-1.11.3.min.js')}}"></script>
@@ -16,40 +17,57 @@
         <script src="{{ asset('js/bootstrap.min.js')}}"></script>
         <script src="{{ asset('js/bootswatch.js')}}"></script>
         <script src="{{ asset('js/Chart.min.js')}}"></script>
-    
+
     </head>
     <body>
     @show
     @section('navbar')
+    <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+			<div class="container">
+				<a class="navbar-brand" href="index.html">Royal<span>estate</span></a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="oi oi-menu"></span> Menu
+				</button>
+
+				<div class="collapse navbar-collapse" id="ftco-nav">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item active"><a href="/" class="nav-link">Home</a></li>
+						<li class="nav-item"><a href="kriteria" class="nav-link">Kriteria</a></li>
+						<li class="nav-item"><a href="alternatif" class="nav-link">Alternatif</a></li>
+						<li class="nav-item"><a href="analisa" class="nav-link">Analisa</a></li>
+						<li class="nav-item"><a href="perhitungan" class="nav-link">Perhitungan</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
     <!-- Static navbar -->
-    <div class="navbar navbar-default">
-        <div class="container-fluid">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#"></a>
-          </div>
-          <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav">
-              <li><a href="home">Home</a></li>
-              <li><a href="kriteria">Data Kriteria</a></li>
-              <li><a href="alternatif">Data Alternatif</a></li>
-			  <li><a href="analisa">Analisa</a></li>
-              <li><a href="perhitungan">Perhitungan</a></li>
-			</ul>
-          </div><!--/.nav-collapse -->
-        </div><!--/.container-fluid -->
-      </div>
+    <!-- <div class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li><a href="/">Home</a></li>
+            <li><a href="kriteria">Data Kriteria</a></li>
+            <li><a href="alternatif">Data Alternatif</a></li>
+            <li><a href="analisa">Analisa</a></li>
+            <li><a href="perhitungan">Perhitungan</a></li>
+          </ul>
+        </div><!--/.nav-collapse
+      </div><!--/.container-fluid
+    </div> -->
       @show
 
         <div class="container">
             @yield('content')
         </div>
 
-        
     </body>
 </html>
